add6f179d2a31a8c0563c856e4fd3dfc8e6a9c90
NIKHIL SHRESTHA <shrestha.nikhil0025@gmail.com>
2024-10-25 18:05:56 +0530

    set up the footer component git status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,17 @@
 import Header from "./component/Header";
+import Footer from "./component/Footer";
+
 function App() {
-  return <Header />;
+  
+  return(
+    <div>
+     <Header />
+     <Footer />
+
+    </div>
+
+  )
+
 }
 
 export default App;
---
+++ frontend/src/component/Footer.js
@@ -0,0 +1,13 @@
+import "../Footer.css";
+import React from "react";
+
+export default function Footer(){
+    return(
+        
+            <div className = "footerbox">
+                <h5>St. No. 10-D,New Dashmesh Nagar, Amritsar Road, Moga</h5>
+            
+            </div>
+       
+    )
+}
