9fb4099bc2f57bc50569f0a935911eb1be585dc4
daksh0110 <dakshmahajan0110@gmail.com>
2024-10-26 13:35:08 +0530

    chnaged header layout for desktop

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "../Header.css";
+import MenuIcon from "../icons/MenuIcon";
 const Header = () => {
   return (
     <div className="header-box">
-      <div className="header-left">
-        <div className="site-logo">LOGO</div>
-        <div className="header-links">
-          <h5>Home</h5> <h5>Contact</h5> <h5>about</h5>{" "}
-        </div>
+      <div className="site-logo"> Logo</div>
+      <div className="header-links">
+        {" "}
+        <h5>Home</h5>
+        <h5>Contact</h5>
+        <h5>About</h5>{" "}
       </div>
       <div className="user-box"></div>
     </div>
