0655b8063263dd20519621decc5f95d35180879a
daksh0110 <dakshmahajan0110@gmail.com>
2024-10-26 20:05:26 +0530

    designing ui for responsive header

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Header.css";
 import MenuIcon from "../icons/MenuIcon";
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
+    console.log(menuOpen);
+  }
   return (
-    <div className="header-box">
+    <nav className="header-box">
       <div className="site-logo"> Logo</div>
       <div className="header-links">
-        {" "}
         <h5>Home</h5>
         <h5>Contact</h5>
         <h5>About</h5>{" "}
       </div>
-      <div className="user-box"></div>
-    </div>
+      <div className="user-box">Login</div>
+      <div className="menu-icon " onClick={() => toggleMenu()}>
+        <MenuIcon />
+      </div>
+    </nav>
   );
 };
 
